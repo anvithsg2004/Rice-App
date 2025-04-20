86a57b17f4d588741441e0db1fa9c70190c46003
anvithsg2004 <anvithsg2004@gmail.com>
2025-04-20 19:13:30 +0530

    Some small Changes

--- rice-frontend/src/api/api.js
+++ rice-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:5173';
+export const API_BASE_URL = 'http://localhost:8080';
 
 // Create a clean axios instance without auth headers
 const plainAxios = axios.create();
